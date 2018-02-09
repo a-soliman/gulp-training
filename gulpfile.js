226376ac22d2ae1318a7ef53f7334ec4aeb5c382
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-09 07:50:27 -0800

    implemnted gulp-pplumber for error handling

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const livereload 	= require('gulp-livereload');
 const concat		= require('gulp-concat');
 const minifyCss		= require('gulp-minify-css');
 const autoprefixer	= require('gulp-autoprefixer');
+const plumber		= require('gulp-plumber');
 
 
 // File psths
@@ -15,6 +16,10 @@ let HTMLS_PATH	= 'public/*.html';
 // Styles
 gulp.task('styles', () => {
 	gulp.src(['public/css/reset.css', STYLES_PATH])
+		.pipe(plumber((err) => {
+			console.log('Styles task error');
+			console.log(err);
+		}))
 		.pipe(autoprefixer())
 		.pipe(concat('styles.css'))
 		.pipe(minifyCss())
